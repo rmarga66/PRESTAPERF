d82e81d4e4534f993b2c266fa7b4bd7e2bd54f9a
rmarga66 <romainmargalet@gmail.com>
2024-12-15 22:46:33 +0100

    main.py

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ sheet_data = [
     {"Indication": "SA", "Designation": "forf instal2, système actif élec, PERFADOM2-I2-SA-ELEC", "Tarif_HT": 137.38},
     {"Indication": "SA", "Designation": "forf ins rempli par ES, syst actif élec, PERFADOM3-I-REMPLI-ES-SA-ELEC", "Tarif_HT": 137.38},
     {"Indication": "DIFF", "Designation": "forf instal1, diffuseur, PERFADOM4-I1-DIFF", "Tarif_HT": 190.81},
-    {"Indication": "DIFF", "Designation": "Perf a dom, forf instal2, diffuseur, PERFADOM5-I2-DIFF", "Tarif_HT": 87.77},
-    {"Indication": "GRAV", "Designation": "Perf a dom, forfait instal et suivi, gravité, PERFADOM6-IS-GRAV", "Tarif_HT": 38.16},
-    {"Indication": "SA", "Designation": "Perf a dom, forfait hebdo suivi, système actif, PERFADOM7E-S-SA-ELEC", "Tarif_HT": 83.95},
-    {"Indication": "DIFF", "Designation": "Perf a dom, forfait hebdo suivi, diffuseur, PERFADOM8-S-DIFF", "Tarif_HT": 38.16},
-    {"Indication": "GRAV", "Designation": "Perf a dom, forf/ perf consom-access, Gravité, < 15 perf, PERFADOM17-C-GRAV < 15/ 28J", "Tarif_HT": 9},
-    {"Indication": "GRAV", "Designation": "Perf a dom, forf hebdo consom-access, Gravité, 1 perf/ j, PERFADOM18-C-GRAV = 1/ J", "Tarif_HT": 63.35},
-    {"Indication": "GRAV", "Designation": "Perf à dom, forf hebdo consom-access, Gravité, 2 perf/ j, PERFADOM19-C-GRAV = 2/ J", "Tarif_HT": 119.83},
-    {"Indication": "GRAV", "Designation": "Perf à dom, forf hebdo consom-access, Gravité, > 2 perf/ j, PERFADOM20-C-GRAV > 2/ J", "Tarif_HT": 170.2},
-    {"Indication": "VVC", "Designation": "Perf à dom, forf d'entret voie centrale, PERFADOM21-ENTRETIEN-VC-SF-PICC", "Tarif_HT": 7.63},
-    {"Indication": "PICC", "Designation": "Perf à dom, forfait d'ent voie centrale, PERFADOM22-ENTRETIEN-VC-PICC-LINE", "Tarif_HT": 14.88},
-    {"Indication": "DIFF", "Designation": "Perf à dom, forf hebdo consom-access, débr diff étab sant, PERFADOM24-C-DEBR-DIFF", "Tarif_HT": 11.5},
-    {"Indication": "SA", "Designation": "Perf a dom, forf hebdo consom-access, systeme actif,1perf/s, PERFA-DOM27-C-SA=1/S", "Tarif_HT": 28.97},
-    {"Indication": "SA", "Designation": "Perf a dom, forf heb consom-access, systeme actif,2a3perf/s, PERFA-DOM28-C-SA=2A3/S", "Tarif_HT": 57.92},
-    {"Indication": "SA", "Designation": "Perf a dom, forf heb consom-access, systeme actif,4a6perf/s, PERFA-DOM29-C-SA=4A6/S", "Tarif_HT": 130.33},
-    {"Indication": "SA", "Designation": "Perf a dom, forf hebdo consom-access, systeme actif,1 perf/j, PERFA-DOM30-C-SA=1/J", "Tarif_HT": 200.12},
-    {"Indication": "SA", "Designation": "Perf a dom, forf hebdo consom-access, systeme actif,2 perf/j, PERFA-DOM31-C-SA=2/J", "Tarif_HT": 379.14},
-    {"Indication": "SA", "Designation": "Perf a dom, forf hebdo consom-access, systeme actif,3 perf/j, PERFA-DOM32-C-SA=3/J", "Tarif_HT": 539.39},
-    {"Indication": "SA", "Designation": "Perf a  dom, forf hebdo consom-access, systeme actif, >3 perf/j, PERFADOM33-C-SA>3/J", "Tarif_HT": 679.32},
-    {"Indication": "DIFF", "Designation": "Perf a  dom, forf hebdo consom-access, diffuseur,1 perf/s, PERFA-DOM34-C-DIFF=1/S", "Tarif_HT": 26.06},
-    {"Indication": "DIFF", "Designation": "Perf a dom, forf heb consom-access, diffuseur,2a3perf/s, PERFADOM35-C-DIFF=2A3/S", "Tarif_HT": 52.12},
-    {"Indication": "DIFF", "Designation": "Perf a dom, forf heb consom-access, diffuseur,4a6perf/s, PERFADOM36-C-DIFF=4A6/S", "Tarif_HT": 117.29},
-    {"Indication": "DIFF", "Designation": "Perf a  dom, forf hebdo consom-access, diffuseur,1 perf/j, PERFA-DOM37-C-DIFF=1/J", "Tarif_HT": 180.1},
-    {"Indication": "DIFF", "Designation": "Perf a  dom, forf hebdo consom-access, diffuseur,2 perf/j, PERFA-DOM38-C-DIFF=2/J", "Tarif_HT": 341.22},
-    {"Indication": "DIFF", "Designation": "Perf a  dom, forf hebdo consom-access, diffuseur,3 perf/j, PERFA-DOM39-C-DIFF=3/J", "Tarif_HT": 485.45},
-    {"Indication": "DIFF", "Designation": "Perf a dom, forf hebdo consom-access, diffuseur, >3 perf/j, PERFA-DOM40-C-DIFF>3/J", "Tarif_HT": 611.38},
-    {"Indication": "IMMUNO SC", "Designation": "Perf a dom, forf heb consom-access, systeme actif,1perf/s, PERFA-DOM41-C-SA IMMU-SC", "Tarif_HT": 39.96},
-    {"Indication": "IMMUNO IV", "Designation": "Perf a dom, forf consom-access, systeme actif,1perf/j, PERFADOM42-C-SA IMMU-IV", "Tarif_HT": 39.96},
-    {"Indication": "SA", "Designation": "Perf à dom, forf hebdo consom-access, débr SA étab sant, PERFADOM45-C-DEBR-SA", "Tarif_HT": 11.5},
+    {"Indication": "DIFF", "Designation": "forf instal2, diffuseur, PERFADOM5-I2-DIFF", "Tarif_HT": 87.77},
+    {"Indication": "GRAV", "Designation": "forfait instal et suivi, gravité, PERFADOM6-IS-GRAV", "Tarif_HT": 38.16},
+    {"Indication": "SA", "Designation": "forfait hebdo suivi, système actif, PERFADOM7E-S-SA-ELEC", "Tarif_HT": 83.95},
+    {"Indication": "DIFF", "Designation": "forfait hebdo suivi, diffuseur, PERFADOM8-S-DIFF", "Tarif_HT": 38.16},
+    {"Indication": "GRAV", "Designation": "forf/ perf consom-access, Gravité, < 15 perf, PERFADOM17-C-GRAV < 15/ 28J", "Tarif_HT": 9},
+    {"Indication": "GRAV", "Designation": "forf hebdo consom-access, Gravité, 1 perf/ j, PERFADOM18-C-GRAV = 1/ J", "Tarif_HT": 63.35},
+    {"Indication": "GRAV", "Designation": "forf hebdo consom-access, Gravité, 2 perf/ j, PERFADOM19-C-GRAV = 2/ J", "Tarif_HT": 119.83},
+    {"Indication": "GRAV", "Designation": "forf hebdo consom-access, Gravité, > 2 perf/ j, PERFADOM20-C-GRAV > 2/ J", "Tarif_HT": 170.2},
+    {"Indication": "VVC", "Designation": "forf d'entret voie centrale, PERFADOM21-ENTRETIEN-VC-SF-PICC", "Tarif_HT": 7.63},
+    {"Indication": "PICC", "Designation": "forfait d'ent voie centrale, PERFADOM22-ENTRETIEN-VC-PICC-LINE", "Tarif_HT": 14.88},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, débr diff étab sant, PERFADOM24-C-DEBR-DIFF", "Tarif_HT": 11.5},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, systeme actif,1perf/s, PERFA-DOM27-C-SA=1/S", "Tarif_HT": 28.97},
+    {"Indication": "SA", "Designation": "forf heb consom-access, systeme actif,2a3perf/s, PERFA-DOM28-C-SA=2A3/S", "Tarif_HT": 57.92},
+    {"Indication": "SA", "Designation": "forf heb consom-access, systeme actif,4a6perf/s, PERFA-DOM29-C-SA=4A6/S", "Tarif_HT": 130.33},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, systeme actif,1 perf/j, PERFA-DOM30-C-SA=1/J", "Tarif_HT": 200.12},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, systeme actif,2 perf/j, PERFA-DOM31-C-SA=2/J", "Tarif_HT": 379.14},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, systeme actif,3 perf/j, PERFA-DOM32-C-SA=3/J", "Tarif_HT": 539.39},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, systeme actif, >3 perf/j, PERFADOM33-C-SA>3/J", "Tarif_HT": 679.32},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, diffuseur,1 perf/s, PERFA-DOM34-C-DIFF=1/S", "Tarif_HT": 26.06},
+    {"Indication": "DIFF", "Designation": "forf heb consom-access, diffuseur,2a3perf/s, PERFADOM35-C-DIFF=2A3/S", "Tarif_HT": 52.12},
+    {"Indication": "DIFF", "Designation": "forf heb consom-access, diffuseur,4a6perf/s, PERFADOM36-C-DIFF=4A6/S", "Tarif_HT": 117.29},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, diffuseur,1 perf/j, PERFA-DOM37-C-DIFF=1/J", "Tarif_HT": 180.1},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, diffuseur,2 perf/j, PERFA-DOM38-C-DIFF=2/J", "Tarif_HT": 341.22},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, diffuseur,3 perf/j, PERFA-DOM39-C-DIFF=3/J", "Tarif_HT": 485.45},
+    {"Indication": "DIFF", "Designation": "forf hebdo consom-access, diffuseur, >3 perf/j, PERFA-DOM40-C-DIFF>3/J", "Tarif_HT": 611.38},
+    {"Indication": "IMMUNO SC", "Designation": "forf heb consom-access, systeme actif,1perf/s, PERFA-DOM41-C-SA IMMU-SC", "Tarif_HT": 39.96},
+    {"Indication": "IMMUNO IV", "Designation": "forf consom-access, systeme actif,1perf/j, PERFADOM42-C-SA IMMU-IV", "Tarif_HT": 39.96},
+    {"Indication": "SA", "Designation": "forf hebdo consom-access, débr SA étab sant, PERFADOM45-C-DEBR-SA", "Tarif_HT": 11.5},
     {"Indication": "NUT PAR", "Designation": "Nutrition parentérale, forfait de première installation", "Tarif_HT": 325},
     {"Indication": "NUT PAR", "Designation": "Nutrition parentérale, forfait d’installation après perfusion à domicile", "Tarif_HT": 144.62},
     {"Indication": "NUT PAR", "Designation": "Nutrition parentérale, sans entérale associée, pompe, prest° hebd., pdt 12 prem. Sem", "Tarif_HT": 75},
@@ -55,8 +55,8 @@ logo = Image.open("logo.png")  # Replace "logo.png" with the path to your logo f
 
 # Streamlit UI
 st.image(logo, width=150)
-st.title("PRESTAPERF Calculator")
-st.sidebar.header("Configuration")
+st.title("Calculette PRESTAPERF by RM")
+st.sidebar.header("START")
 
 # Session state initialization
 if "details" not in st.session_state:
@@ -67,14 +67,14 @@ if "client_sap" not in st.session_state:
     st.session_state.client_sap = ""
 
 # Indications Selection
-selected_indications = st.sidebar.multiselect("Choisissez les indications", options=indications)
+selected_indications = st.sidebar.multiselect("Choisissez les dispositifs du patient", options=indications)
 
 # Filtered Designations
 designations = [item for item in sheet_data if item["Indication"] in selected_indications]
 designation_quantities = {}
 
 if selected_indications:
-    st.subheader("Désignations")
+    st.subheader("PERFADOM")
     for item in designations:
         designation = item["Designation"]
         quantity = st.number_input(f"Quantité pour {designation}", min_value=0, step=1, key=designation)
@@ -101,6 +101,7 @@ if st.session_state.details:
     st.subheader("Total")
     st.write(f"Total HT: {st.session_state.total:.2f}€")
     st.write("Fait par Romain Margalet avec ❤️")
+    st.write("ALLEZ L'OM")
 
 
 
