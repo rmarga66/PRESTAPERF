26043dbe0bf0da52a4314813f1c1749807b91479
rmarga66 <romainmargalet@gmail.com>
2024-12-15 22:52:21 +0100

    main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ if st.session_state.details:
     st.subheader("Total")
     st.write(f"Total HT: {st.session_state.total:.2f}€")
     st.write("Fait par Romain Margalet avec ❤️")
-    st.write("Visca Catalunya !")
+    st.write("Si problème romain.margalet@bastide-medical.fr")
 
 
 
